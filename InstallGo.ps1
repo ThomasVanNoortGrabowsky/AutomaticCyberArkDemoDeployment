f5fcd1bf9a33f45c4bf63ad096cc122865ff0cc4
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-04-17 17:45:18 +0200

    Update InstallGo.ps1

--- InstallGo.ps1
+++ InstallGo.ps1
@@ -24,21 +24,25 @@ function Test-IsAdmin {
 # Self‑elevate if not running as admin
 if (-not (Test-IsAdmin)) {
     Write-Host "Not running as Administrator. Relaunching elevated..."
-    Start-Process -FilePath pwsh -ArgumentList "-NoProfile","-ExecutionPolicy","Bypass","-File","`"$PSCommandPath`"","-Version",$Version,"-Gopath","`"$Gopath`"" -Verb RunAs
+    Start-Process -FilePath pwsh `
+        -ArgumentList "-NoProfile","-ExecutionPolicy","Bypass","-File","`"$PSCommandPath`"","-Version",$Version,"-Gopath","`"$Gopath`"" `
+        -Verb RunAs
     exit
 }
 
 # 1) Download MSI
-$msiName = "go$Version.windows-amd64.msi"
+$msiName     = "go$Version.windows-amd64.msi"
 $downloadUrl = "https://go.dev/dl/$msiName"
-$msiPath    = Join-Path $env:TEMP $msiName
+$msiPath     = Join-Path $env:TEMP $msiName
 
 Write-Host "Downloading Go $Version from $downloadUrl..."
 Invoke-RestMethod -Uri $downloadUrl -OutFile $msiPath -UseBasicParsing
 
 # 2) Silent install
 Write-Host "Installing Go silently..."
-Start-Process msiexec.exe -ArgumentList "/i","`"$msiPath`"","/qn","/norestart" -Wait
+Start-Process msiexec.exe `
+    -ArgumentList "/i","`"$msiPath`"","/qn","/norestart" `
+    -Wait
 
 # Clean up
 Remove-Item $msiPath -Force
@@ -48,7 +52,8 @@ Write-Host "Setting user environment variable GOPATH = $Gopath"
 [Environment]::SetEnvironmentVariable("GOPATH", $Gopath, "User")
 
 # 4) Update User PATH
-$envPaths = [Environment]::GetEnvironmentVariable("Path","User").Split(";",[StringSplitOptions]::RemoveEmptyEntries)
+$envPaths = [Environment]::GetEnvironmentVariable("Path","User") `
+    .Split(";",[StringSplitOptions]::RemoveEmptyEntries)
 $goBins   = @("C:\Program Files\Go\bin", (Join-Path $Gopath "bin"))
 
 foreach ($p in $goBins) {
@@ -60,25 +65,7 @@ foreach ($p in $goBins) {
 $newPath = $envPaths -join ";"
 [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
 
-# 5) Broadcast WM_SETTINGCHANGE so new consoles pick it up
-Write-Host "Broadcasting environment change..."
-$signature = @"
-using System;
-using System.Runtime.InteropServices;
-public class EnvNotify {
-    [DllImport("user32.dll",SetLastError=true)]
-    public static extern IntPtr SendMessageTimeout(
-        IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
-        uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
-}
-"@
-Add-Type $signature
-[Void][EnvNotify]::SendMessageTimeout(
-    [IntPtr]0xffff, 0x1A, [UIntPtr]0, "Environment",
-    0x0002, 5000, [ref]([UIntPtr]0)
-)
-
-# 6) Refresh current session & verify
+# 5) Refresh current session & verify
 $env:GOPATH = $Gopath
 $env:PATH   = $newPath
 
