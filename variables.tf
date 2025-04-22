597756bcb1e94d35b16c71d19779560229d4c8ca
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-04-22 10:56:50 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "vmrest_user" {
+  description = "Username created by vmrest.exe --config"
+  type        = string
+  default     = "vmrest"
+}
+
+variable "vmrest_password" {
+  description = "Password created by vmrest.exe --config"
+  type        = string
+}
+
+variable "base_vm_id" {
+  description = "ID of an existing VM to clone (from the REST API /vms list)"
+  type        = string
+}
+
+variable "vm_processors" {
+  description = "Number of vCPUs for the new VM"
+  type        = number
+  default     = 2
+}
+
+variable "vm_memory" {
+  description = "Memory (MB) for the new VM"
+  type        = number
+  default     = 2048
+}
+
+variable "vm_path" {
+  description = "Filesystem path where the new VM will be created"
+  type        = string
+  default     = "C:\\Users\\<YourName>\\Documents\\Virtual Machines\\TestVM-Terraform"
+}
