a8ee163c2e6bdc88cf66389f58008ace2dcdf91e
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-04-30 14:07:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vmworkstation = {
       source  = "elsudano/vmworkstation"
-      version = "1.1.6"
+      version = "1.0.4"
     }
   }
 }
@@ -10,46 +10,41 @@ terraform {
 provider "vmworkstation" {
   user     = var.vmrest_user
   password = var.vmrest_password
-  url      = "http://127.0.0.1:8697"    # no /api
+  url      = "http://127.0.0.1:8697"   # no /api suffix :contentReference[oaicite:7]{index=7}
   https    = false
   debug    = true
 }
 
-# Vault
 resource "vmworkstation_vm" "vault" {
   sourceid     = var.vault_image_id
   denomination = "Vault-VM"
-  description  = "CyberArk Vault"
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
-# PVWA
 resource "vmworkstation_vm" "pvwa" {
   sourceid     = var.app_image_id
   denomination = "PVWA-VM"
-  description  = "CyberArk PVWA"
+  depends_on   = [vmworkstation_vm.vault]
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
-# PSM
 resource "vmworkstation_vm" "psm" {
   sourceid     = var.app_image_id
   denomination = "PSM-VM"
-  description  = "CyberArk PSM"
+  depends_on   = [vmworkstation_vm.pvwa]
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
-# CPM
 resource "vmworkstation_vm" "cpm" {
   sourceid     = var.app_image_id
   denomination = "CPM-VM"
-  description  = "CyberArk CPM"
+  depends_on   = [vmworkstation_vm.psm]
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
