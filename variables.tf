e4328aff9503c8a714a3719b6e24604b5852d198
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-04-30 13:04:32 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,38 +1,32 @@
 variable "vmrest_user" {
-  description = "Username for vmrest API"
-  type        = string
-  default     = "vmrest"
+  type    = string
+  default = "vmrest"
 }
 
 variable "vmrest_password" {
-  description = "Password for vmrest API"
-  type        = string
+  type    = string
+  default = "Cyberark1"
 }
 
 variable "vault_image_id" {
-  description = "ID of the Vault golden VM image"
-  type        = string
+  type    = string
 }
 
 variable "app_image_id" {
-  description = "ID of the golden image for PVWA/CPM/PSM"
-  type        = string
+  type    = string
 }
 
 variable "vm_processors" {
-  description = "Number of vCPUs"
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "vm_memory" {
-  description = "Memory in MB"
-  type        = number
-  default     = 2048
+  type    = number
+  default = 2048
 }
 
 variable "vm_path" {
-  description = "Path for the new VM"
-  type        = string
-  default     = "C:\\Users\\<YourName>\\Documents\\Virtual Machines\\TestVM-Terraform"
+  type    = string
+  default = "C:\\Users\\ThomasvanNoort\\Documents\\Virtual Machines\\"
 }
