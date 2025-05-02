d0a5945e1b138200c7eae57868210f876bc9fab6
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-05-02 13:29:46 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,7 @@
-variable "vmrest_user" {
-  type        = string
-  description = "VMREST username"
-}
-
-variable "vmrest_pass" {
-  type        = string
-  description = "VMREST password"
-  sensitive   = true
-}
-
-variable "template_id" {
-  type        = string
-  description = "GUID of the Win2022_GUI template VM"
-}
-
-variable "vm_processors" {
-  type        = number
-  description = "Number of vCPUs for each demo VM"
-  default     = 2
-}
-
-variable "vm_memory" {
-  type        = number
-  description = "Memory (MB) for each demo VM"
-  default     = 2048
-}
-
-variable "vm_path" {
-  type        = string
-  description = "Filesystem path where the new VMs will be placed"
-}
+# variables.tf
+variable "vmrest_user" { type = string }
+variable "vmrest_pass" { type = string }
+variable "template_id" { type = string }
+variable "vm_processors" { type = number; default = 2 }
+variable "vm_memory"     { type = number; default = 2048 }
+variable "vm_path"       { type = string }
