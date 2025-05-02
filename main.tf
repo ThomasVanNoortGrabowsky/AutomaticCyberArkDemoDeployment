a2128c47ed483e543d079df7ed0b00debf570dd3
ThomasVanNoortGrabowsky <van.noort@grabowsky.com>
2025-05-02 11:01:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,49 +2,53 @@ terraform {
   required_providers {
     vmworkstation = {
       source  = "elsudano/vmworkstation"
-      version = "1.0.4"
+      version = "1.1.6"
     }
   }
 }
 
 provider "vmworkstation" {
   user     = var.vmrest_user
-  password = var.vmrest_password
-  url      = "http://127.0.0.1:8697"   # no /api suffix :contentReference[oaicite:7]{index=7}
+  password = var.vmrest_pass
+  url      = "http://127.0.0.1:8697"
   https    = false
-  debug    = true
+  debug    = false
 }
 
 resource "vmworkstation_vm" "vault" {
-  sourceid     = var.vault_image_id
+  sourceid     = var.template_id
   denomination = "Vault-VM"
+  linked_clone = true
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
 resource "vmworkstation_vm" "pvwa" {
-  sourceid     = var.app_image_id
+  sourceid     = var.template_id
   denomination = "PVWA-VM"
   depends_on   = [vmworkstation_vm.vault]
+  linked_clone = true
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
 resource "vmworkstation_vm" "psm" {
-  sourceid     = var.app_image_id
+  sourceid     = var.template_id
   denomination = "PSM-VM"
   depends_on   = [vmworkstation_vm.pvwa]
+  linked_clone = true
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
 }
 
 resource "vmworkstation_vm" "cpm" {
-  sourceid     = var.app_image_id
+  sourceid     = var.template_id
   denomination = "CPM-VM"
   depends_on   = [vmworkstation_vm.psm]
+  linked_clone = true
   processors   = var.vm_processors
   memory       = var.vm_memory
   path         = var.vm_path
